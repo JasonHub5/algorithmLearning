15a99292dcdd5a1b2e6fc4655f3e2356cfb75168
Jason.Jiang <jingfengkongjian@qq.com>
2019-11-08 13:58:34 +0800

    init repository

---
+++ src/main/java/sort/QuickSort.java
@@ -0,0 +1,98 @@
+package sort;
+
+public class QuickSort {
+
+    /**
+     * 快速排序
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @return
+     */
+    public static int[] quickSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return arr;
+        }
+
+        int pivot = partition2(arr, left, right);
+        quickSort(arr, left, pivot - 1);
+        quickSort(arr, pivot + 1, right);
+
+        return arr;
+    }
+
+    /**
+     * find pivot （从右边开始遍历）
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @return
+     */
+    public static int partition1(int[] arr, int left, int right) {
+        int pivot = arr[left];
+        int i = left + 1;
+        int j = right;
+
+        while (i < j) {
+            //从右边开始
+            while (i < j && arr[j] >= pivot) {
+                j--;
+            }
+            //再遍历左边
+            while (i < j && arr[i] <= pivot) {
+                i++;
+            }
+
+            if (i >= j) {
+                break;
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        arr[left] = arr[j];
+        arr[j] = pivot;
+
+        return j;
+    }
+
+    /**
+     * find pivot （从左边开始遍历）
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @return
+     */
+    public static int partition2(int[] arr, int left, int right) {
+        int pivot = arr[left];
+        int i = left + 1;
+        int j = right;
+
+        while (i < j) {
+            //从左边开始
+            while (i <= j && arr[i] <= pivot) {
+                i++;
+            }
+            //再遍历右边
+            while (i <= j && arr[j] >= pivot) {
+                j--;
+            }
+
+            if (i >= j) {
+                break;
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        arr[left] = arr[j];
+        arr[j] = pivot;
+
+        return j;
+    }
+}
---
+++ src/test/java/SortTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import sort.QuickSort;
+
+public class SortTest {
+
+    @Test
+    public void quickSortTest() {
+        int[] arr = {10, 23, 2, 4, 5, 7, 24, 1};
+
+        int[] ints = QuickSort.quickSort(arr, 0, arr.length - 1);
+
+        for (int i : ints) {
+            System.out.println(i);
+        }
+        System.out.println(arr.toString());
+    }
+}
